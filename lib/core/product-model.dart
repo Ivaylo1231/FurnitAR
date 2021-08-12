8d6b462eb41d153c0606199624e633a8f93b835d
Ivaylo Bankov <ivaylo1231@gmail.com>
2021-08-13 00:15:57 +0300

    [feat] products json parsing

--- lib/core/product-model.dart
+++ lib/core/product-model.dart
@@ -1,5 +1,7 @@
+import 'package:ar_flutter_plugin/datatypes/node_types.dart';
 import 'package:ar_flutter_plugin/models/ar_node.dart';
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart';
 
 class ProductModel {
   final String name;
@@ -13,4 +15,35 @@ class ProductModel {
     required this.image,
     required this.model,
   });
+  
+  @override
+  factory ProductModel.fromMap(Map<String, dynamic> map) {
+    String name = map['name'];
+    String price = map['price'];
+    Image image = map['isLocal']
+      ? Image(image: AssetImage(map['imagePath']))
+      : Image.network(map['imagePath']);
+    ARNode model = ARNode(
+      type: map['isLocal'] ? NodeType.localGLTF2 : NodeType.webGLB,
+      uri: map['modelPath'], 
+      scale: Vector3(0.2, 0.2, 0.2),
+      position: Vector3(0.0, 0.0, 0.0),
+      rotation: Vector4(1.0, 0.0, 0.0, 0.0),
+    );
+
+    return ProductModel(
+      name: name,
+      price: price,
+      image: image,
+      model: model
+    );
+  }
+
+  static List<ProductModel> fromMapList(List<Map<String, dynamic>> mapList) {
+    List<ProductModel> products = [];
+    mapList.forEach((mapItem) {
+      products.add(ProductModel.fromMap(mapItem));
+    });
+    return products;
+  }
 }
--- lib/screens/home/widgets/product-list.dart
+++ lib/screens/home/widgets/product-list.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:ar_flutter_plugin/datatypes/node_types.dart';
 import 'package:ar_flutter_plugin/models/ar_node.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:furnitar/core/product-model.dart';
 import 'package:furnitar/screens/home/widgets/product-item.dart';
 import 'package:vector_math/vector_math_64.dart';
@@ -13,32 +16,38 @@ class ProductList extends StatefulWidget {
 }
 
 class _ProductListState extends State<ProductList> {
+  Future<List<ProductModel>> _getProducts() async {
+    return ProductModel.fromMapList(
+      List<Map<String, dynamic>>.from(
+        jsonDecode(await rootBundle.loadString('assets/products.json')),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: GridView.count(
-        primary: false,
-        padding: const EdgeInsets.all(20),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        crossAxisCount: 2,
-        children: <Widget>[
-          ProductItem(
-            productModel: ProductModel(
-              name: "Omles",
-              price: "\$0.99",
-              image: Image(image: AssetImage('assets/omles.png')),
-              model: ARNode(
-                type: NodeType.webGLB,
-                uri: "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF-Binary/Duck.glb",
-                scale: Vector3(0.2, 0.2, 0.2),
-                position: Vector3(0.0, 0.0, 0.0),
-                rotation: Vector4(1.0, 0.0, 0.0, 0.0),
-              ),
-            ),
+    return FutureBuilder<List<ProductModel>>(
+      future: _getProducts(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return CircularProgressIndicator();
+        }
+
+        return Expanded(
+          child: GridView.count(
+            primary: false,
+            padding: const EdgeInsets.all(20),
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            crossAxisCount: 2,
+            children: snapshot.data!
+              .map<Widget>(
+                (productModel) => ProductItem(productModel: productModel),
+              )
+            .toList(),
           ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
